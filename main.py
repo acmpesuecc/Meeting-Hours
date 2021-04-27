fa80ecbc9c3d29fab5f43e9ef59e0e3094ca76cf
Navin Shrinivas <karupal2002@gmail.com>
2021-04-27 21:10:34 +0530

    added poll and unpoll
    Signed-off-by: Navin Shrinivas <karupal2002@gmail.com>
    
    we are using tasks decorator connected to discord.py.ext

--- main.py
+++ main.py
@@ -3,20 +3,32 @@ import os
 import decouple
 from dotenv import load_dotenv
 from discord.utils import get
+from discord.ext import tasks
 
-intents = discord.Intents.all()
-client = discord.Client(intents=intents)
+
+client = discord.Client()
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
+
+recursionflag=0
 @client.event
 async def on_ready():
     print('I AM IN THE MATRIX [logged in]')
 
 @client.event
 async def on_message(message):
+    global spam_loop
     if message.author == client.user:
         return
+    if message.content.startswith('-poll'):
+        spam_loop.start(message)
+    if message.content.startswith('-unpoll'):
+        spam_loop.cancel()
+        
+@tasks.loop()
+async def spam_loop(message):
+    await message.channel.send("recurse")
 
 client.run(TOKEN)
