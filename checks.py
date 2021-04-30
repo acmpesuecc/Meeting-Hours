79fdea21f9cc7176000d863dce14133d86bb9682
Navin Shrinivas <karupal2002@gmail.com>
2021-04-30 14:12:49 +0530

    changing -help command
    Signed-off-by: Navin Shrinivas <karupal2002@gmail.com>
    
    changing -help command to use embeds....honeslty its nicer looking

---
+++ checks.py
@@ -0,0 +1,3 @@
+from pymongo import MongoClient
+
+def linkfn(message,coll)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from discord.utils import get
 from discord.ext import tasks
 from pymongo import MongoClient
+# import linkwaiter
 
 
 client = discord.Client()
@@ -15,28 +16,36 @@ coll = db["polledData"]
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
+global link_loop
 
-recursionflag=0
+
+#helper_docs
+embed=discord.Embed(title="Helper", url="https://del.dog/cruxagrori.txt", description="This should give you a fair idea as to how to use the bot:", color=discord.Color.red())
+embed.add_field(name="-poll", value="Execute this when you want the bot to start monitoring the meeting.", inline=False)
+embed.add_filed(name="-unpoll", vaule="Execute this command when you want the bot to stop monitoring your meeting", inline=True)
+
 @client.event
 async def on_ready():
     print('I AM IN THE MATRIX [logged in]')
-    #coll.insert_one({"test": ["https://www.google.com", "https://www.twitter.com"]}) {was for test use only!}
+    #coll.insert_one({"test": ["https://www.google.com", "https://www.twitter.com"]}) {was for test use only!} dbstructure of saving links
 @client.event
 async def on_message(message):
-    global spam_loop
     if message.author == client.user:
         return
     if message.content.startswith('-poll'):
-        spam_loop.start(message)
+        link_loop.start()
     if message.content.startswith('-unpoll'):
-        spam_loop.cancel()
-    if message.content.startswith('-help'):
-        message.channel.send("-poll => starts recording the minutes of the meeting and searchs for links \n-unpoll => stops collection loop")
+        link_loop.cancel()
+    if message.content.startswith("-help"):
+        await message.channel.send(embed=embed)
         
 @tasks.loop()
-async def spam_loop(message):
-    await message.channel.send("recurse")
-
+async def link_loop():
+    message = await client.wait_for("message", check=lambda message: message.author == message.author)
+    await message.channel.send(message.content)
+    # print(message)
+    # if linker.linkfn(coll,message) :
+        
 client.run(TOKEN)
 
 '''
